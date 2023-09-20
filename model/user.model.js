f8efe38fc1c6e768df315dbf362ce1fa817cc6fa
Diwakar Sharma <diwakarsharma2216@gmail.com>
2023-09-20 13:22:44 +0530

    update-3

--- model/user.model.js
+++ model/user.model.js
@@ -5,12 +5,7 @@ const mongoose=require("mongoose")
 const userSchema=mongoose.Schema({
     name:String,
     email:String,
-    gender:String,
-    password:String,
-    age:Number,
-    city:String,
-    is_married:Boolean
-
+     password:String,
 })
 
 const UserModel=mongoose.model("user",userSchema)
